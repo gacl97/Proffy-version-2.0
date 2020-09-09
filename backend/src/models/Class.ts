21ccb6a3240348a691ed79300fe66e559e94aa38
Gustavo Augusto <gacl@ic.ufal.br>
2020-09-09 10:11:50 -0300

    Class Model

---
+++ backend/src/models/Class.ts
@@ -0,0 +1,37 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import User from './User';
+
+@Entity('classes')
+class Class {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  subject: string;
+
+  @Column('float')
+  cost: number;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Class;
