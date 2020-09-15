3e128a31970a519f0edc0ef5dd575a2164d477a2
Gustavo Augusto <gacl@ic.ufal.br>
2020-09-15 10:53:33 -0300

    React's initial structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Routes from './routes';
+import GlobalStyles from './styles/GlobalStyles';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Router>
+        <Routes />
+
+        <GlobalStyles />
+      </Router>
+    </>
+  );
+};
+
+export default App;
