f37a29ccebdd43ec0d36acc93ccc192deac79734
Gustavo Augusto <gacl@ic.ufal.br>
2020-09-15 11:00:34 -0300

    Add conection with db, add cors, middleware of errors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,31 +1,40 @@
 import 'reflect-metadata';
 import express, { Response, Request, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import routes from './routes';
 import AppError from './errors/AppError';
+
+// Injection dependencies
+import './container';
+// Db connection
+import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 // Middleware of Errors
-app.use((error: Error, request: Request, response: Response, _: NextFunction) => {
-  if (error instanceof AppError) {
-    return response.json(error.statusCode).json({
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.json(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.log(error);
+
+    return response.status(500).json({
       status: 'error',
-      message: error.message,
-    })
-  }
-
-  console.log(error);
-
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
-})
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('Servidor iniciado na porta: 3333');
-})
+});
